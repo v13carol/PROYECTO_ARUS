6448733da9bf6f6bcb38cb8661aacd21e9a80d77
Ehyska <ehyskagutierrez@gmail.com>
2018-05-20 17:40:35 -0500

    cuarto commit

--- app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.print ("segundo cambio prueba 2");
 
         System.out.print ("Commit 3 prueba 3");
+        System.out.print ("commit 4 prueba 4");
     }
 }
