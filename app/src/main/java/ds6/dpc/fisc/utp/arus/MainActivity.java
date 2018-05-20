c13b33f6627968066e11329e40ae3006ac760f1a
v13carol <carolv3913@gmail.com>
2018-05-20 17:45:38 -0500

    quinto cambio en mainActivity

--- app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
@@ -17,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
 >>>>>>> Stashed changes*/
 
         System.out.print ("segundo cambio prueba 2");
-
         System.out.print ("Commit 3 prueba 3");
         System.out.print ("commit 4 prueba");
+
+        System.out.print ("commit 4 prueba222");
+
+
     }
 }
