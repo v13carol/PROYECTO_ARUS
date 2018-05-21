1d4ecfc9e372329224ec8edb700308970250f854
v13carol <carolv3913@gmail.com>
2018-05-20 19:01:33 -0500

    imprimir en MainActivity

--- app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+            // CAMBIO UNO
+        System.out.print("hola mundo!! 1");
     }
 }
