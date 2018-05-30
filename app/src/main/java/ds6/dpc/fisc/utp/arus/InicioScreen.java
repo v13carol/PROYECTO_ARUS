e926f9c276d6ec5e74d842d41731a2f5f1944e0e
v13carol <carolv3913@gmail.com>
2018-05-30 02:10:03 -0500

    actividad inicio, donde en el java creo un intent para mandarlo a la actividad Main

--- app/src/main/java/ds6/dpc/fisc/utp/arus/InicioScreen.java
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/InicioScreen.java
@@ -1,5 +1,7 @@
 package ds6.dpc.fisc.utp.arus;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class InicioScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (InicioScreen.this, MainActivity.class);
+                startActivity(intent);
+            }        }, 4000);
     }
 }
