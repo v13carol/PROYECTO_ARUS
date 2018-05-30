8ed74661eb668fbf487f8070e327d836be0f81af
v13carol <carolv3913@gmail.com>
2018-05-30 00:41:50 -0500

    arregle el nombre del icono y cree una actividad nueva que es la pantalla de inicio que se mostrará

---
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/InicioScreen.java
@@ -0,0 +1,13 @@
+package ds6.dpc.fisc.utp.arus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_screen);
+    }
+}
