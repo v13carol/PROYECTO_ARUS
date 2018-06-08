e3ce320bcbdf84c755a4f585f4b6d6f5ed956a5b
Ehyska <ehyskagutierrez@gmail.com>
2018-06-08 01:13:59 -0500

    creación dd los 2 ultimos fragments

---
+++ app/src/main/java/ds6/dpc/fisc/utp/arus/Fragment5.java
@@ -0,0 +1,20 @@
+package ds6.dpc.fisc.utp.arus;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Fragment5 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
